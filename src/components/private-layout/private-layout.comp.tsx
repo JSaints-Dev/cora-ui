b76b6a7247c00d8e194d11d4a5e4de4b13254bcd
Jean Saints <57118447+JSaints-Dev@users.noreply.github.com>
2024-11-19 15:50:59 -0300

    fix: fix on clicked logo

--- src/components/private-layout/private-layout.comp.tsx
+++ src/components/private-layout/private-layout.comp.tsx
@@ -6,7 +6,7 @@ export function PrivateLayout({children, onLogoClick, actions}: PrivateLayoutPro
     <div className="flex flex-col h-screen w-full">
       <div className='flex w-full bg-brand-primary h-16 justify-center'>
         <div className='flex items-center w-full justify-between px-5 max-w-screen-lg'>
-          <div data-logo-click={onLogoClick} className='py-2 w-28 data-[logo-click]:cursor-pointer'>
+          <div data-logo-click={!!onLogoClick} onClick={onLogoClick} className='py-2 w-28 data-[logo-click]:cursor-pointer'>
             <Logo variant='full'/>
           </div>
           {actions}
